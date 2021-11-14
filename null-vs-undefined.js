04d10dacea0ceed1d39b1d0b037b716c423048e7
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-11-14 15:29:27 +0600

    NULL vs UNDEFINED

---
+++ null-vs-undefined.js
@@ -0,0 +1,27 @@
+//----Undefined ----------
+
+let name;
+
+function add(num1, num2) {
+  console.log(num1 + num2);
+  return;
+}
+
+function add(num1, num2) {
+  console.log(num1, num2);
+}
+
+const premik = { name: "Taufiq", phone: 12456 };
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [12, 12, 23];
+console.log(ages[11]);
+
+//---------------NULL --------------
+// NULL is an empty or non-existent value.
+//NULL must be assigned
+// let a = null;
+// console.log(a);
