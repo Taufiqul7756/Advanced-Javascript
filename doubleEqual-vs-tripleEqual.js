626f5b890ce17ebf9065dd8c9fc1889f4f119622
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-11-14 15:42:56 +0600

    == VS ===

---
+++ doubleEqual-vs-tripleEqual.js
@@ -0,0 +1,12 @@
+//== means -> didnot check dataType .
+//=== means -> check data type and value
+
+// const num1 = 0;
+// const num2 = true;
+const num1 = 12;
+const num2 = "12";
+if (num1 === num2) {
+  console.log("Conditions is true");
+} else {
+  console.log("conditions is false");
+}
